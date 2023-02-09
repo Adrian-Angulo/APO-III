7d84a3ced504ad5a451f51cb5b72039723f99f66
Adrian Castillo <adrianangulo1080@gmail.com>
2023-02-09 14:18:00 -0500

    Clase perro

---
+++ Perro.java
@@ -0,0 +1,17 @@
+class Perro{
+    private String nombre; 
+    private String color; 
+    private float peso;
+    private float tamanio;
+    
+    public Perro(String nombre,String color,float peso,float tamanio){
+    this.nombre = nombre;
+    this.color = color;
+    this.peso = peso;
+    this. tamanio = tamanio;
+    
+    
+    }
+
+}
+
