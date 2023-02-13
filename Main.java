02d9a7acbd0b99b42a5b7b17378cef5275b3d333
RMJuanEsteban2 <125038566+RMJuanEsteban2@users.noreply.github.com>
2023-02-13 17:32:52 -0500

    Actualizacion en main
    se añadieron las clases

--- Main.java
+++ Main.java
@@ -6,9 +6,10 @@ public class Main {
         //objecto perro
         Perro perro = new Perro("Lucas", "Negro", 10, 40);
         //Crea objecto gato
-
+        Gato gato = new Gato ("Paco", "Blanco", 4, 25);
         //funcion menu
         Menu(sc, perro);
+        Menu(sc, gato);
         
     }
     //Funcion menuPerro
@@ -41,12 +42,43 @@ public class Main {
                 break;
             default:
                 System.out.println("opcion incorrecta");
-            
+                break;
+        }
+        //Funcion menuGato
+    public static void MenuGato(Scanner sc, Gato gato){
+        
+        System.out.print("""
+            --------------------Gato---------------------
+                1. Jugar
+                2. Saltar
+                3. Peso
+                4. Color
+                5. Tamañio
+                Digite la opcion que desea realizar: """);
+        int opc = sc.nextInt();
+        switch (opc) {
+            case 1:
+                gato.JugarGato();
+                break;
+            case 2: 
+                gato.SaltarGato();
+                break;
+            case 3: 
+                gato.PesoGato();
+                break;
+            case 4:
+                gato.ColorGato();
+                break;
+            case 5: 
+                gato.TamanioGato();
+                break;
+            default:
+                System.out.println("opcion incorrecta");
                 break;
         }
     }
     //Funcion Menu
-    public static void Menu(Scanner sc, Perro perro){
+    public static void Menu(Scanner sc, Perro perro, Gato gato){
         System.out.println("1. Perro");
         System.out.println("2. Gato");
         System.out.print("Digite la opcion que desea realizar: ");
@@ -57,9 +89,11 @@ public class Main {
             break;
             case 2:
             //ingresar MenuGato
+            MenuGato(sc, gato)
             break;
             default:
             System.out.println("opcion incorrecta");
         }
     }
 }
+
