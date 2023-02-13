f6546feb9c4919b3321e246842b82e057b5ef584
EsneydrIbarra <124367643+EsneydrIbarra@users.noreply.github.com>
2023-02-13 13:45:36 -0500

    Actualizacion de la clase Gato.

--- Gato.java
+++ Gato.java
@@ -1,4 +1,50 @@
 class Gato{
+    private String nombre; 
+    private String color; 
+    private float peso;
+    private float tamanio;
     
+    public Gato(String nombre,String color,float peso,float tamanio){
+    this.nombre = nombre;
+    this.color = color;
+    this.peso = peso;
+    this. tamanio = tamanio;
+    
+    
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public float getTamanio() {
+        return tamanio;
+    }
+    
+    //metodos
+
+    public void JugarGato(){
+        System.out.println("el Gato "+ getNombre()+" esta jugando");
+    }
+    public void SaltarGato(){
+        System.out.println("el Gato "+getNombre()+" esta saltado");
+    }
+    public void PesoGato(){
+        System.out.println("El Gato pesa "+ getPeso()+" kg");
+    }
+    public void ColorGato(){
+        System.out.println("el Gato "+getNombre() +" es de color "+ getColor());
+    }
+    public void TamanioGato(){
+        System.out.println("el Gato "+ getNombre()+" mide "+getTamanio() +" centimetros de altura");
+    }
 
 }
